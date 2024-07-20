733e1b69f61929e3b24201996bdd4a49947c406c
marcus aurelio <marcusaurelio743@gmail.com>
2024-07-20 11:51:46 -0300

    criando classes de dominio da aplicação

--- br.comHelpDesk/src/main/java/br/comHelpDesk/domain/Cliente.java
+++ br.comHelpDesk/src/main/java/br/comHelpDesk/domain/Cliente.java
@@ -3,16 +3,28 @@ package br.comHelpDesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import br.comHelpDesk.domain.enums.Perfil;
+
+@Entity
 public class Cliente extends Pessoa {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<Chamado>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 		
 	}
 
 	public Cliente(Long id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 		
 	}
 
