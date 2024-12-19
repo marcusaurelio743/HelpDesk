46cc541b665303fbdee72bf232b8dfa33bdbb9bd
marcus aurelio <marcusaurelio743@gmail.com>
2024-12-19 09:11:32 -0300

    atualizando

--- helpdesk/src/main/java/br/com/helpdesk/HelpDeskApplication.java
+++ helpdesk/src/main/java/br/com/helpdesk/HelpDeskApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelpDeskApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelpDeskApplication.class,args );
+		SpringApplication.run(HelpDeskApplication.class,args ); 
 
 	}
 
