73ece4f3750f1217c791c001b843052c3714235f
marcus aurelio <marcusaurelio743@gmail.com>
2024-12-19 20:45:48 -0300

    criando classes de dominio

--- helpdesk/src/main/java/br/com/helpdesk/domain/Tecnico.java
+++ helpdesk/src/main/java/br/com/helpdesk/domain/Tecnico.java
@@ -3,15 +3,26 @@ package br.com.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import br.com.helpdesk.domain.enums.Perfil;
+@Entity
 public class Tecnico extends Pessoa {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<Chamado>();
 
 	public Tecnico() {
 		super();
+		AddPerfis(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Long id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		AddPerfis(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
