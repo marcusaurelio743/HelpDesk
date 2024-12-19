83e1c84666ec32efd0c132d1f2ac9f70d883fdfd
marcus aurelio <marcusaurelio743@gmail.com>
2024-12-19 14:58:39 -0300

    criando classes domain do projeto

---
+++ helpdesk/src/main/java/br/com/helpdesk/domain/Pessoa.java
@@ -0,0 +1,113 @@
+package br.com.helpdesk.domain;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import br.com.helpdesk.domain.enums.Perfil;
+
+public abstract class Pessoa {
+	protected Long id;
+	protected String nome;
+	protected String cpf;
+	protected String email;
+	protected String senha;
+	protected List<Integer> perfis = new ArrayList<>();
+	protected LocalDate dataCriacao = LocalDate.now();
+	
+	public Pessoa() {
+		super();
+		AddPerfis(Perfil.CLIENTE);
+		
+	}
+
+	public Pessoa(Long id, String nome, String cpf, String email, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		AddPerfis(Perfil.CLIENTE);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Perfil> getPerfis() {
+		return perfis.stream().map(x ->Perfil.toEnum(x)).collect(Collectors.toList());
+	}
+
+	public void AddPerfis(Perfil perfil) {
+		this.perfis.add(perfil.getCodigo());
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	
+	
+
+}
